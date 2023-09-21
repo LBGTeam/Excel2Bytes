322e4f192acc49077e2f595ebf7d64dd562b3669
LianBai <841774407@qq.com>
2023-09-21 18:24:41 +0800

    :bug:修复路径问题

--- Excel2Bytes.py
+++ Excel2Bytes.py
@@ -1,6 +1,12 @@
+import sys
+
 from GenerateUtil import ExportAllData, ExportExtraLNG, FirstExportProject
 
 if __name__ == '__main__':
-    ExportAllData()
-    # ExportExtraLNG()
-    # FirstExportProject()
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '-a':
+            ExportAllData()
+        elif sys.argv[1] == '-e':
+            ExportExtraLNG()
+    else:
+        FirstExportProject()
